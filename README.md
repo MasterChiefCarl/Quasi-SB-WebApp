a7c2938878a4a1c9ff4b01fa1500926d668af086
Got some faces to punch -MC <carlgarces9@gmail.com>
2022-06-13 23:52:28 +0800

    Checkpoint
    Changing based on instruction
    making a welcome page then a main menu

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Quasi-SB-WebApp
+
+A Starbucks-like web application that has these goals
+
+* To create an order like system where the user orders and selects from the kiosk-web like application
+* To be able to access the database and devide the items based on the category
+* To be able to integrate the use of PHP, mySql, and Axios
+
+
+Created by: Web Development 2 Student from USJR
+
+* Garces, Carl Benedict (BSIT 3)
+* Jungco, Angelika (BSIT 3)
+
+* Lapiz, Jaime (BSIT 3)
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>QSASI-STARBUCKS POS</title>
+  <link rel="stylesheet" href="styles.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <link rel="shortcut icon" type="image/x-icon" href="assets/logo/starbucks.png" />
+</head>
+
+<body>
+  <div class="default-layout-body">
+    <div class="nav">
+      <div class="nav-body">navbar</div>
+    </div>
+    <div class="content">
+      <div class="content-body">
+        <div class="welcome">
+          <h1>Welcome To Starbucks</h1>
+          <h3>Please Write Your Name</h3>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">Another label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Place Your Name Here">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="footer-body">
+        <h3>Made with 💖 by Group 1 Jungco, Lapiz, Garces</h3>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
